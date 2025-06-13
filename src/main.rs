964de85756011cfb9672b399cfe49ac2793c77f4
dwarftee <64mrnoah@gmail.com>
2025-06-13 20:45:44 +0100

    commit :0

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,24 @@ use macroquad::prelude::*;
 
 #[macroquad::main("people simulation")]
 async fn main() {
-   loop {
-     clear_background(BLACK);
+    
+    let tst1 = load_texture("assets/bazinga.png").await.unwrap();
+    tst1.set_filter(FilterMode::Nearest);
 
-     next_frame().await;
-   }
+    loop {
+        clear_background(SKYBLUE);
+
+        draw_texture_ex(
+            &tst1,
+            100.0,
+            100.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(200.0,200.0)),
+                ..Default::default()
+            },
+        );
+
+        next_frame().await;
+    }
 }
