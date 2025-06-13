f8fa4ae1117467f9b6f6d2b8f41998049132d154
dwarftee <64mrnoah@gmail.com>
2025-06-13 20:51:56 +0100

    commit :0

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ async fn main() {
     let tst1 = load_texture("assets/bazinga.png").await.unwrap();
     tst1.set_filter(FilterMode::Nearest);
 
+    let x = 100.0;
+    let y = 100.0;
+
     loop {
         clear_background(SKYBLUE);
 
         draw_texture_ex(
             &tst1,
-            100.0,
-            100.0,
+            x,
+            y,
             WHITE,
             DrawTextureParams {
                 dest_size: Some(Vec2::new(200.0,200.0)),
